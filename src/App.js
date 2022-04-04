0665802d0cb800be87f173f22085893e5382b70d
alaminhossainjibon <mdalamin7622@gmail.com>
2022-04-04 14:07:45 +0600

    navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Header from './components/Header/Header';
+import Navbar from './components/Navbar/Navbar';
 import Product from './components/Product/Product';
 
 function App() {
   return (
     <div className="App">
+      <Navbar></Navbar>
       <Header></Header>
       <Product></Product>
     </div>
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Header = () => {
     return (
         <div>
-            <h1 className='text-6xl font-bold text-lime-900'>QUICK BIKE REVIEWS</h1>
+            <h1 className='text-6xl my-5 font-bold text-lime-900'>QUICK BIKE REVIEWS</h1>
         </div>
     );
 };
